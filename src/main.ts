cc18aa76693cc0289b9d9cded4f736e756f6329a
karstenrivas <karsten.rivas@gmail.com>
2026-04-25 17:50:10 -0500

    adding homepage and menu

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+  ],
+}).catch((err) => console.error(err));
